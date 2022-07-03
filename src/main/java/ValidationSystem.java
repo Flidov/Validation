0589185a59baa50121b8337e0d82b9aa0996d9a8
flidov <mrfioden@gmail.com>
2022-07-03 22:25:44 +0300

    Done

--- src/main/java/ValidationSystem.java
+++ src/main/java/ValidationSystem.java
@@ -1,11 +1,34 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class ValidationSystem <T>{
-    private T enter;
-    public static <T> void validate(T enter) throws ValidationFailedException{
+    protected T enter;
 
+    private ValidationSystem(T enter) {
+        this.enter = enter;
     }
-    public static <T> void  validateInt(T enter) throws ValidationFailedException{
 
+    public static <T > void validate(T enter) throws ValidationFailedException{
+      if(enter.getClass()==Integer.class){
+        validateInt((Integer) enter);
+      }
+      if (enter.getClass()==String.class)
+          validateString((String) enter);
+    }
+
+    public static int validateInt(int enter) throws ValidationFailedException {
+        if (enter>0&&enter<11){
+            return enter;
+        }
+        else throw new ValidationFailedException("не верный интервал");
+    }
+    public static String   validateString (String enter) throws ValidationFailedException {
+        Pattern pattern = Pattern.compile("^[A-Z]");
+        Matcher matcher = pattern.matcher(enter);
+        if (matcher.find())
+        return enter;
+     else throw new ValidationFailedException("начинаеться с маленькой буквы");
     }
 }
