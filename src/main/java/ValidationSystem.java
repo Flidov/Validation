f031f1b3727e8589523bdbba48a1a7e39c378e5a
flidov <mrfioden@gmail.com>
2022-07-03 21:13:39 +0300

    New

---
+++ src/main/java/ValidationSystem.java
@@ -0,0 +1,11 @@
+import java.util.stream.Stream;
+
+public class ValidationSystem <T>{
+    private T enter;
+    public static <T> void validate(T enter) throws ValidationFailedException{
+
+    }
+    public static <T> void  validateInt(T enter) throws ValidationFailedException{
+
+    }
+}
